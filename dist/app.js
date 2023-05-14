28729d727e0270f129e05e4412833baeeaac7279
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-14 20:07:26 +0200

    trying to get it to work

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,22 @@
-"use strict";
+fetch('./src/app/config/config-validator.js')
+  .then(response => response.text())
+  .then(text => eval(text))
+  .then(ConfigValidator => {
+    return fetch('./src/app/chatBot/chatbot.js')
+      .then(response => response.text())
+      .then(text => {
+        eval(text);
+        return ConfigValidator.readConfig('./config.json')
+          .then(config => new TwitchChatBot(config).launch());
+      });
+  });
+
+
+
+/*"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_validator_1 = require("./src/app/config/config-validator");
 const chatbot_1 = require("./src/app/chatBot/chatbot");
 config_validator_1.ConfigValidator.readConfig(('./config.json'))
-    .then((config) => new chatbot_1.TwitchChatBot(config).launch());
+    .then((config) => new chatbot_1.TwitchChatBot(config).launch());*/
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsd0VBQW9FO0FBQ3BFLHVEQUEwRDtBQUcxRCxrQ0FBZSxDQUFDLFVBQVUsQ0FBQyxDQUFDLGVBQWUsQ0FBQyxDQUFDO0tBQzVDLElBQUksQ0FBQyxDQUFDLE1BQXFCLEVBQUUsRUFBRSxDQUFFLElBQUksdUJBQWEsQ0FBQyxNQUFNLENBQUMsQ0FBQyxNQUFNLEVBQUUsQ0FBQyxDQUFDIn0=
