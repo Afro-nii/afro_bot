45c0a74f425413a931213553d339fdacf2616ccb
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-14 19:04:29 +0200

    Delete dist directory

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_validator_1 = require("./src/app/config/config-validator");
-const chatbot_1 = require("./src/app/chatBot/chatbot");
-config_validator_1.ConfigValidator.readConfig(('./config.json'))
-    .then((config) => new chatbot_1.TwitchChatBot(config).launch());
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsd0VBQW9FO0FBQ3BFLHVEQUEwRDtBQUcxRCxrQ0FBZSxDQUFDLFVBQVUsQ0FBQyxDQUFDLGVBQWUsQ0FBQyxDQUFDO0tBQzVDLElBQUksQ0FBQyxDQUFDLE1BQXFCLEVBQUUsRUFBRSxDQUFFLElBQUksdUJBQWEsQ0FBQyxNQUFNLENBQUMsQ0FBQyxNQUFNLEVBQUUsQ0FBQyxDQUFDIn0=
