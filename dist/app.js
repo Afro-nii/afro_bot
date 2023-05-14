d40e3248b6ae1e40097f80165d97102eb9b16846
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-14 20:51:06 +0200

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,8 @@
-fetch('./src/app/config/1config-validator.js')
+fetch('./dist/src/app/config/1config-validator.js')
   .then(response => response.text())
   .then(text => eval(text))
   .then(ConfigValidator => {
-    return fetch('./src/app/chatBot/chatbot.js')
+    return fetch('./dist/src/app/chatBot/chatbot.js')
       .then(response => response.text())
       .then(text => {
         eval(text);
