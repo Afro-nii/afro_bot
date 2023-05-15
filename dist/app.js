3473044eb42cf1aaaae93111d162d7be3016f9d4
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-15 16:34:07 +0200

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -10,13 +10,18 @@
           .then(config => new TwitchChatBot(config).launch());
       });
   });*/
+  import { ChatBotConfig } from './src/app/config/config.model';
+  import { ConfigValidator } from './src/app/config/config-validator';
+  import { TwitchChatBot } from './src/app/chatBot/chatbot';
+  
+  ConfigValidator.readConfig('./config.json')
+    .then((config) => new TwitchChatBot(config).launch())
 
-
-
+/*
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_validator_1 = require("./dist/src/app/config/config-validator");
 const chatbot_1 = require("./dist/src/app/chatBot/chatbot");
 config_validator_1.ConfigValidator.readConfig(('./config.json'))
-    .then((config) => new chatbot_1.TwitchChatBot(config).launch());
+    .then((config) => new chatbot_1.TwitchChatBot(config).launch());*/
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsd0VBQW9FO0FBQ3BFLHVEQUEwRDtBQUcxRCxrQ0FBZSxDQUFDLFVBQVUsQ0FBQyxDQUFDLGVBQWUsQ0FBQyxDQUFDO0tBQzVDLElBQUksQ0FBQyxDQUFDLE1BQXFCLEVBQUUsRUFBRSxDQUFFLElBQUksdUJBQWEsQ0FBQyxNQUFNLENBQUMsQ0FBQyxNQUFNLEVBQUUsQ0FBQyxDQUFDIn0=
