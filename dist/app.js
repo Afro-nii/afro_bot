68e8f1980fc2148dfc03f674ad2146349309b45a
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-15 16:30:12 +0200

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,4 @@
-fetch('./dist/src/app/config/1config-validator.js')
+/*fetch('./dist/src/app/config/1config-validator.js')
   .then(response => response.text())
   .then(text => eval(text))
   .then(ConfigValidator => {
@@ -9,14 +9,14 @@ fetch('./dist/src/app/config/1config-validator.js')
         return ConfigValidator.readConfig('./config.json')
           .then(config => new TwitchChatBot(config).launch());
       });
-  });
+  });*/
 
 
 
-/*"use strict";
+"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const config_validator_1 = require("./src/app/config/config-validator");
-const chatbot_1 = require("./src/app/chatBot/chatbot");
+const config_validator_1 = require("./dist/src/app/config/config-validator");
+const chatbot_1 = require("./dist/src/app/chatBot/chatbot");
 config_validator_1.ConfigValidator.readConfig(('./config.json'))
-    .then((config) => new chatbot_1.TwitchChatBot(config).launch());*/
+    .then((config) => new chatbot_1.TwitchChatBot(config).launch());
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsd0VBQW9FO0FBQ3BFLHVEQUEwRDtBQUcxRCxrQ0FBZSxDQUFDLFVBQVUsQ0FBQyxDQUFDLGVBQWUsQ0FBQyxDQUFDO0tBQzVDLElBQUksQ0FBQyxDQUFDLE1BQXFCLEVBQUUsRUFBRSxDQUFFLElBQUksdUJBQWEsQ0FBQyxNQUFNLENBQUMsQ0FBQyxNQUFNLEVBQUUsQ0FBQyxDQUFDIn0=
