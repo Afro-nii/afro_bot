4fef7be2e1c89f43ddb586b2a4de99d7d0e3a982
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-18 17:27:41 +0200

    Update app.js

--- build/app.js
+++ build/app.js
@@ -1,4 +1,4 @@
-import { ConfigValidator } from './app/config/config-validator.js';
-import { TwitchChatBot } from './app/chatBot/chatbot.js';
+import { ConfigValidator } from './app/config/config-validator';
+import { TwitchChatBot } from './app/chatBot/chatbot';
 ConfigValidator.readConfig(('./config.json'))
     .then(function (config) { return new TwitchChatBot(config).launch(); });
