1c054b77595bc245394fe2d28cc41a46fe0b80db
Afro-nii <122732753+Afro-nii@users.noreply.github.com>
2023-05-19 03:28:41 +0200

    its 3 am. please just work at this point

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-    input: 'app.ts', // Replace 'src/index.ts' with the path to your TypeScript entry point
+    input: 'src/app.ts', // Replace 'src/index.ts' with the path to your TypeScript entry point
     output: {
       file: 'build/bundle.js', // Replace 'dist/bundle.js' with the desired output file path
       format: 'umd', // Choose the desired output format (e.g., 'umd', 'cjs', 'esm')
       name: 'Afro_Bot', // Replace 'MyApp' with the name of your application/library
     },
     plugins: [typescript()],
-  };
+}
+//"exclude": ["./built/**/*"]
 
 /*export default {
    // input: 'src/app.ts',
